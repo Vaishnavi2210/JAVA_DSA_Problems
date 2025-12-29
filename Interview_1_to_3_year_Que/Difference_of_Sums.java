9a1bd5db614ba9b8b2bc7800a444e394c3a1ba6f
Vaishnavi2210 <nerkarvaishnavi2210@gmail.com>
2025-12-30 01:04:38 +0530

    Added Interview questions folder for 1-3 years experience

---
+++ Interview_1_to_3_year_Que/Difference_of_Sums.java
@@ -0,0 +1,20 @@
+public class Difference_of_Sums {
+    /*
+       Given two numbers n and m, calculate the sum of all multiples of n or
+       m below some limit or sum of numbers in some pattern, so that for n=4, m=10 the output is 22.
+
+       n = 4, m = 10 → output = 22
+     */
+    public static void main(String[] args) {
+        int n = 4;
+        int m = 10;
+        int sum = 0;
+
+        for(int i =1; i <= m; i++){
+            if(i % n == 0 || i % m==0 ){
+                sum =sum + i;
+            }
+        }
+        System.out.println(" Output "+ sum);
+    }
+}
