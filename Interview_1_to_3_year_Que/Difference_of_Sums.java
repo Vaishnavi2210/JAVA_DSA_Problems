da67af5161df532d25e5f3902cf32333281a29c5
Vaishnavi2210 <nerkarvaishnavi2210@gmail.com>
2025-12-30 17:36:01 +0530

    Added DSA solutions

--- Interview_1_to_3_year_Que/Difference_of_Sums.java
+++ Interview_1_to_3_year_Que/Difference_of_Sums.java
@@ -2,19 +2,29 @@ public class Difference_of_Sums {
     /*
        Given two numbers n and m, calculate the sum of all multiples of n or
        m below some limit or sum of numbers in some pattern, so that for n=4, m=10 the output is 22.
-
+       Return sum1 - sum2.
        n = 4, m = 10 → output = 22
      */
     public static void main(String[] args) {
-        int n = 4;
-        int m = 10;
-        int sum = 0;
+       int n = 4;
+       int m = 10;
+       //int sum = 0;
+        int sum1 = 0, sum2 =0;
 
-        for(int i =1; i <= m; i++){
-            if(i % n == 0 || i % m==0 ){
-                sum =sum + i;
+        for(int i =1; i<= m; i++){
+          /*  if(i % n ==0 || i % m == 0){
+               sum += i;
+              }
+          */
+           if(i % n == 0){
+               sum1 = sum1 + i;
+           }
+           sum2 = sum2 + i;if(i % m == 0){
+               sum2 = sum2+ i;
             }
-        }
-        System.out.println(" Output "+ sum);
+       }
+       // System.out.println(" Output "+ sum);
+        System.out.println(sum1 + sum2); // 22  correct output
+        System.out.println(sum1 - sum2);  // 2  wrong output
     }
 }
